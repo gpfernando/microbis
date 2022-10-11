7c676334cbf162737de458a8630cd3cf4d8f33d9
Gene Fernando <gpfernando@roadrunner.com>
2022-10-11 16:32:43 -0700

    slides

--- src/components/slides/index.js
+++ src/components/slides/index.js
@@ -6,9 +6,9 @@ import { Colors } from "../../styles/theme";
 
 
 const slides = [
-     "/slides/slide1.jpeg",
-     "/slides/slide2.jpeg",
-     "/slides/slide3.jpeg",
+     "/slides/slide1.jpg",
+     "/slides/slide2.jpg",
+     "/slides/slide3.jpg",
      "/slides/Slide4.jpeg",
      "/slides/slide5.jpeg",
      "/slides/slide6.png",
